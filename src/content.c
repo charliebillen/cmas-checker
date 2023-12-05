96753368bef2587ceeaea65db7b2d295cb776977
Charlie Billen <charlesbillen@gmail.com>
2023-12-05 20:12:12 +0000

    Fixes Content-Length header

--- src/content.c
+++ src/content.c
@@ -17,8 +17,8 @@ void write_ct_header(int fd)
 
 void write_cl_header(int fd)
 {
-    // 64 bytes for now, yolo
-    write(fd, "Content-Length: 64\n", 19);
+    // both responses are 19 chars
+    write(fd, "Content-Length: 19\n", 19);
 }
 
 void write_content(int fd)
@@ -30,7 +30,7 @@ void write_content(int fd)
 
     if (dt->tm_mday == 25 && dt->tm_mon == 11)
     {
-        write(fd, "It's Christmas!\n", 16);
+        write(fd, "It's Christmas!!!!\n", 19);
     }
 
     write(fd, "It's not Christmas\n", 19);
